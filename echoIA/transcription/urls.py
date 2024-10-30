d8b472057df17de7b514cf896497dd4c8c307764
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2024-10-30 17:41:13 -0300

    Atualização das URLs
    Atualizado as URLs para indicar o caminho para Login

--- echoIA/transcription/urls.py
+++ echoIA/transcription/urls.py
@@ -1,10 +1,14 @@
 # transcriber/urls.py
-from django.urls import path
+
 from .views import RecordView, StopRecordingView, TranscribeView, index
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('start-recording/', RecordView.as_view(), name='start-recording'),
     path('stop-recording/', StopRecordingView.as_view(), name='stop-recording'),
     path('transcribe/', TranscribeView.as_view(), name='transcribe'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
