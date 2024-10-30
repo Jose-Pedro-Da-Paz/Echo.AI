f36713f2ef71a79a57b9f990a08e9369ac100dc7
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2024-10-30 17:41:57 -0300

    Criação do Model
    Responsável por consultar o Banco de Dados

--- echoIA/transcription/models.py
+++ echoIA/transcription/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transcription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    audio_file = models.FileField(upload_to='audio_files/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Transcription by {self.user.username} on {self.created_at}"
