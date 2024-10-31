099fa156ad8fc6c813537b7d778d60586e82de72
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2024-10-31 08:31:24 -0300

    Estruturação Front-End

--- echoIA/transcription/urls.py
+++ echoIA/transcription/urls.py
@@ -1,5 +1,5 @@
-# transcriber/urls.py
-
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import RecordView, StopRecordingView, TranscribeView, index
 from django.contrib import admin
 from django.urls import path, include
@@ -10,5 +10,9 @@ urlpatterns = [
     path('start-recording/', RecordView.as_view(), name='start-recording'),
     path('stop-recording/', StopRecordingView.as_view(), name='stop-recording'),
     path('transcribe/', TranscribeView.as_view(), name='transcribe'),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# Adiciona URLs para arquivos estáticos em modo de desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL)
