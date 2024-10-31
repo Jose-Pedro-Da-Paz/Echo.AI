eedc16823b183f6d2e2c79d94e506dfc779473c7
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2024-10-31 08:30:21 -0300

    Estruturação Front-End

--- echoIA/transcription/models.py
+++ echoIA/transcription/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Transcription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transcriptions')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    audio_file = models.FileField(upload_to='audio_files/', null=True, blank=True)
 
     def __str__(self):
-        return f"Transcription by {self.user.username} on {self.created_at}"
+        return f"Transcrição por {self.user.username} em {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
