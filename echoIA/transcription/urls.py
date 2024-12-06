e8266c4dc67ad7928470e99d380f380b7c893b73
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2024-12-06 19:39:16 -0300

    Adição do sistema de edição avançado

--- echoIA/transcription/urls.py
+++ echoIA/transcription/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import RecordView, StopRecordingView, TranscribeView, index, folder_list, create_folder, TranscriptionListView, SaveTranscriptionView
+from .views import RecordView, StopRecordingView, TranscribeView, index, folder_list, create_folder, TranscriptionListView, SaveTranscriptionView, editor_view, save_updates_view, get_concatenated_transcriptions, save_concatenated_transcriptions
 from django.contrib import admin
 from django.urls import path, include
 
@@ -15,6 +15,10 @@ urlpatterns = [
     path('folders/<int:folder_id>/transcriptions/', TranscriptionListView.as_view(), name='transcription-list'),
     path('create-folder/', create_folder, name='create-folder'),
     path('save-transcription/', SaveTranscriptionView.as_view(), name='save_transcription'),
+    path('editor/<int:folder_id>/', editor_view, name='editor'),
+    path('save-updates/', save_updates_view, name='save_updates'),
+    path('editor/<int:folder_id>/concatenated/', get_concatenated_transcriptions, name='get_concatenated_transcriptions'),
+    path('save-concatenated/', save_concatenated_transcriptions, name='save_concatenated_transcriptions'),
     
 ]
 
